594b94e4c8122223a3fe540a801433afefe76351
Axel Berres <Axel.Berres@dlr.de>
2016-04-19 10:50:36 +0200

    - add project explorer - try to add Registry editor

--- development/de.symo.application/src/de/symo/application/emfstore/LocationProvider.java
+++ development/de.symo.application/src/de/symo/application/emfstore/LocationProvider.java
@@ -5,7 +5,7 @@ import java.io.File;
 import org.eclipse.emf.emfstore.server.ESLocationProvider;
 
 public class LocationProvider implements ESLocationProvider {
-	public static final Object CDT_FOLDERNAME = ".cdt";
+	public static final Object SYMO_FOLDERNAME = ".symo";
 	
 	public LocationProvider() {
 	}
@@ -15,7 +15,7 @@ public class LocationProvider implements ESLocationProvider {
 		StringBuffer sb = new StringBuffer();
 		sb.append(System.getProperty("user.home"));
 		sb.append(File.separatorChar);
-		sb.append(CDT_FOLDERNAME);
+		sb.append(SYMO_FOLDERNAME);
 
 		return sb.toString();
 	}
