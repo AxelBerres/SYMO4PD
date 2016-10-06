87b263da3cac52fdc55d9addeaaa2e038ee1288b
Axel Berres <Axel.Berres@dlr.de>
2016-10-06 17:06:45 +0200

    - add FileExplorer Part

---
+++ development/de.symo.application/src/de/symo/application/e4/parts/ViewContentProvider.java
@@ -0,0 +1,44 @@
+package de.symo.application.e4.parts;
+
+import java.io.File;
+
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+
+class ViewContentProvider implements ITreeContentProvider {
+
+	@Override
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		System.out.println("Changed");
+	}
+
+	@Override
+	public void dispose() {			
+	}
+
+	@Override
+	public boolean hasChildren(Object element) {
+		File file = (File) element;
+		return file.isDirectory();
+	}
+
+	@Override
+	public Object getParent(Object element) {
+		File file = (File) element;
+		return file.getParentFile();
+	}
+
+	@Override
+	public Object[] getElements(Object inputElement) {
+		File file = (File) inputElement;
+
+		return file.listFiles();
+	}
+
+	@Override
+	public Object[] getChildren(Object parentElement) {
+		File file = (File) parentElement;
+		return file.listFiles();
+
+	}
+}
