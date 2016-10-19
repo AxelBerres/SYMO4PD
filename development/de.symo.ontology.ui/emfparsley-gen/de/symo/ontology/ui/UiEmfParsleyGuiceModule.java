15a00b2e8db993e7d520952a0eb2c4dac3eb7373
Michael Sh <michael.shamiyeh@bauhaus-luftfahrt.net>
2016-10-19 16:37:14 +0200

    ontology library: intermediate commit;

--- development/de.symo.ontology.ui/emfparsley-gen/de/symo/ontology/ui/UiEmfParsleyGuiceModule.java
+++ development/de.symo.ontology.ui/emfparsley-gen/de/symo/ontology/ui/UiEmfParsleyGuiceModule.java
@@ -1,14 +1,24 @@
 package de.symo.ontology.ui;
 
+import com.google.inject.Provider;
 import de.symo.ontology.ui.binding.UiFormControlFactory;
 import de.symo.ontology.ui.edit.action.UiMenuBuilder;
 import de.symo.ontology.ui.resource.UiResourceManager;
+import de.symo.ontology.ui.ui.provider.UiFeatureCaptionProvider;
+import de.symo.ontology.ui.ui.provider.UiFeaturesProvider;
 import de.symo.ontology.ui.ui.provider.UiLabelProvider;
+import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.parsley.EmfParsleyGuiceModule;
 import org.eclipse.emf.parsley.composite.FormControlFactory;
+import org.eclipse.emf.parsley.edit.IEditingStrategy;
+import org.eclipse.emf.parsley.edit.UndoableEditingStrategy;
 import org.eclipse.emf.parsley.edit.action.EditingMenuBuilder;
+import org.eclipse.emf.parsley.edit.domain.GlobalAdapterFactoryEditingDomainProvider;
 import org.eclipse.emf.parsley.resource.ResourceManager;
+import org.eclipse.emf.parsley.ui.provider.FeatureCaptionProvider;
+import org.eclipse.emf.parsley.ui.provider.FeaturesProvider;
 import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.swt.SWT;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 @SuppressWarnings("all")
@@ -18,8 +28,33 @@ public class UiEmfParsleyGuiceModule extends EmfParsleyGuiceModule {
   }
   
   @Override
+  public Class<? extends IEditingStrategy> bindIEditingStrategy() {
+    return UndoableEditingStrategy.class;
+  }
+  
+  @Override
+  public int valueTreeFormSashStyle() {
+    return SWT.HORIZONTAL;
+  }
+  
+  @Override
+  public Class<? extends Provider<AdapterFactoryEditingDomain>> provideAdapterFactoryEditingDomain() {
+    return GlobalAdapterFactoryEditingDomainProvider.class;
+  }
+  
+  @Override
   public Class<? extends ILabelProvider> bindILabelProvider() {
     return UiLabelProvider.class;
+  }
+  
+  @Override
+  public Class<? extends FeatureCaptionProvider> bindFeatureCaptionProvider() {
+    return UiFeatureCaptionProvider.class;
+  }
+  
+  @Override
+  public Class<? extends FeaturesProvider> bindFeaturesProvider() {
+    return UiFeaturesProvider.class;
   }
   
   @Override
--- development/de.symo.ontology.ui/emfparsley-gen/de/symo/ontology/ui/binding/UiFormControlFactory.java
+++ development/de.symo.ontology.ui/emfparsley-gen/de/symo/ontology/ui/binding/UiFormControlFactory.java
@@ -4,26 +4,44 @@ import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.emf.parsley.composite.FormControlFactory;
 import org.eclipse.emf.parsley.util.DatabindingUtil;
-import org.eclipse.jface.databinding.swt.ISWTObservableValue;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 @SuppressWarnings("all")
 public class UiFormControlFactory extends FormControlFactory {
-  public Control control_OntologyRepository_password(final DataBindingContext dataBindingContext, final IObservableValue observableValue) {
-    Control control = createControl_OntologyRepository_password();
+  public Control control_OntologyLibrary_activeOntology(final DataBindingContext dataBindingContext, final IObservableValue observableValue) {
+    Control control = createControl_OntologyLibrary_activeOntology();
     dataBindingContext.bindValue(
-    	createTarget_OntologyRepository_password(control),
+    	createTarget_OntologyLibrary_activeOntology(control),
     	observableValue);
     return control;
   }
   
-  protected Control createControl_OntologyRepository_password() {
-    return this.createText("", SWT.PASSWORD);
+  protected Control createControl_OntologyLibrary_activeOntology() {
+    Composite _parent = this.getParent();
+    final Composite c = new Composite(_parent, SWT.NONE);
+    GridLayout _gridLayout = new GridLayout(2, false);
+    c.setLayout(_gridLayout);
+    final Text t = this.createText(c, SWT.BORDER);
+    final Button b = new Button(c, SWT.PUSH);
+    b.setText("Push me");
+    final Listener _function = (Event it) -> {
+      Shell _shell = c.getShell();
+      MessageDialog.openInformation(_shell, "Event", "You pushed me!");
+    };
+    b.addListener(SWT.MouseDown, _function);
+    return t;
   }
   
-  protected IObservableValue createTarget_OntologyRepository_password(final Control it) {
-    ISWTObservableValue _observeText = DatabindingUtil.observeText(it, SWT.Modify);
-    return _observeText;
+  protected IObservableValue createTarget_OntologyLibrary_activeOntology(final Control it) {
+    return DatabindingUtil.observeText(it);
   }
 }
