dee4bce12065e53a617ea02b09880bfd913f0d2e
Michael Shamiyeh <michael.shamiyeh@bauhaus-luftfahrt.net>
2017-01-20 09:03:30 +0100

    de.symo.service clean-up; added plugin de.symo.oida.changehandler;

---
+++ development/de.symo.oida.changehandler/src/de/symo/oida/changehandler/SymoChangeHandler.java
@@ -0,0 +1,18 @@
+package de.symo.oida.changehandler;
+
+import org.eclipse.emf.common.notify.Notification;
+
+import oida.bridge.observerservice.changehandler.AbstractChangeHandler;
+
+public class SymoChangeHandler extends AbstractChangeHandler {
+
+	public SymoChangeHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void handleAdd(Notification notification) {
+		// TODO Auto-generated method stub
+		
+	}
+}
