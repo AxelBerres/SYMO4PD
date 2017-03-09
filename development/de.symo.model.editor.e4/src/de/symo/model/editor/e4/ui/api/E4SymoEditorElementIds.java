5e833fa979515f816e557566e524cc15dc32413f
Michael Sh <michael.shamiyeh@bauhaus-luftfahrt.net>
2017-03-09 12:08:59 +0100

    added api class with contants for e4 part descriptors;

---
+++ development/de.symo.model.editor.e4/src/de/symo/model/editor/e4/ui/api/E4SymoEditorElementIds.java
@@ -0,0 +1,13 @@
+package de.symo.model.editor.e4.ui.api;
+
+/**
+ * 
+ * @author Michael.Shamiyeh
+ * @since 2017-03-09
+ *
+ */
+public final class E4SymoEditorElementIds {
+	public static final String SYMOMODEL_EDITOR_PARTDESCRIPTOR_ID = "de.symo.model.editor.e4.partdescriptor.symomodeleditor";
+	public static final String REGISTRY_EDITOR_PARTDESCRIPTOR_ID = "de.symo.model.editor.e4.partdescriptor.registryeditorpart";
+	public static final String USECASE_EDITOR_PARTDESCRIPTOR_ID = "de.symo.model.editor.e4.partdescriptor.usecaseeditor";
+}
