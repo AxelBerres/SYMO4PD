34266bb28a3f7086fb63416a50f37fb3cfb676a4
Axel Berres <Axel.Berres@dlr.de>
2017-04-25 08:02:34 +0200

    - implement multiple selection deletion of files

--- development/de.symo.projectbrowser.e4/src/de/symo/projectbrowser/e4/ui/parts/IDecoration.java
+++ development/de.symo.projectbrowser.e4/src/de/symo/projectbrowser/e4/ui/parts/IDecoration.java
@@ -3,6 +3,5 @@ package de.symo.projectbrowser.e4.ui.parts;
 import org.eclipse.swt.graphics.ImageData;
 
 public interface IDecoration {
-
 	public void registerFileIcon(final String extension, final ImageData imageData);
 }
